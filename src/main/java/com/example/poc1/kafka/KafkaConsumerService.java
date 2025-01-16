f4b653874757fd943fa0e0e28808e41fc339a822
SohiniBanik <sohinibanik2000@gmail.com>
2025-01-16 12:46:47 +0530

    Added Comments

--- src/main/java/com/example/poc1/kafka/KafkaConsumerService.java
+++ src/main/java/com/example/poc1/kafka/KafkaConsumerService.java
@@ -25,18 +25,22 @@ public class KafkaConsumerService {
     private final KafkaConsumer<String,String> kafkaConsumer;
 
     public KafkaConsumerService(@Value("${spring.kafka.bootstrap-servers}")String bootstrapServers, @Value("${spring.kafka.consumer.group-id}") String groupId) {
+        //config
         Properties props = new Properties();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, LogMessageDeSerializer.class);
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        //consumer client
         this.kafkaConsumer = new KafkaConsumer<>(props);
     }
 
     public void consume(String topic) {
+        //subscribe to topics
         kafkaConsumer.subscribe(Collections.singletonList(topic));
 
+        //polling for message
         try{
             while(true){
                 ConsumerRecords<String, String> records = kafkaConsumer.poll(Duration.ofMillis(1000));
